02ed334e9facbeb6efb146b833fea0516dafa6d3
kodsaeid-cmd <kod.saeid@gmail.com>
2026-07-29 12:45:30 +0330

    feat: add admin users page

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,10 @@
+import { AdminUsersDashboard } from "@/components/admin-users-dashboard";
+import { AppShell } from "@/components/app-shell";
+
+export default function AdminUsersPage() {
+  return (
+    <AppShell>
+      <AdminUsersDashboard />
+    </AppShell>
+  );
+}
