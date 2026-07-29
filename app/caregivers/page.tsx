d91ec841735eaaf28312f36a31394ff227f1fe0b
kodsaeid-cmd <kod.saeid@gmail.com>
2026-07-29 12:51:53 +0330

    feat: add caregivers page

---
+++ app/caregivers/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/app-shell";
+import { CaregiversDashboard } from "@/components/caregivers-dashboard";
+
+export default function CaregiversPage() {
+  return (
+    <AppShell>
+      <CaregiversDashboard />
+    </AppShell>
+  );
+}
