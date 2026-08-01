5f392a8175626808f241e9baf65f0a7f459f8862
kodsaeid-cmd <kod.saeid@gmail.com>
2026-08-01 15:00:19 +0330

    fix: keep paginated caregiver router stable

---
+++ preview/caregiver-directory-router-guard.js
@@ -0,0 +1,13 @@
+(()=>{
+'use strict';
+if(window.__salamatCaregiverDirectoryRouterGuardV1)return;
+window.__salamatCaregiverDirectoryRouterGuardV1=true;
+function mark(){
+  const current=window.renderModule;
+  if(typeof current==='function'&&current.__salamatCaregiverPaginationV1){
+    current.__salamatDirectoryV2=true;
+  }
+}
+mark();
+setInterval(mark,250);
+})();
