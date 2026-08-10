183256e15bf3f891d16ad2de008008d1f7692a1b
kodsaeid-cmd <kod.saeid@gmail.com>
2026-08-10 17:14:38 +0330

    feat: add admin credential card and Jalali evaluation dates on desktop

--- desktop-react/entry.tsx
+++ desktop-react/entry.tsx
@@ -1,3 +1,5 @@
+import "../shared/staff-credential-runtime-v1";
+import "../shared/evaluation-jalali-runtime-v1";
 import React from "react";
 import {createRoot} from "react-dom/client";
 import {DesktopStaffApp} from "./app";
