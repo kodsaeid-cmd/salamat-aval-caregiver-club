e3c8dd2e55bda569e57d33d43b49a31756ed662a
kodsaeid-cmd <kod.saeid@gmail.com>
2026-08-12 10:00:40 +0330

    feat: load contract welcome on caregiver entry

--- mobile-react/caregiver-entry-v5.tsx
+++ mobile-react/caregiver-entry-v5.tsx
@@ -3,4 +3,5 @@ import "../shared/caregiver-mobile-scorecard-tabs-v1";
 import "./mobile-ambient-background-v1.css";
 import "./caregiver-scorecard-tabs-v1.css";
 import "./caregiver-v4";
+import "./caregiver-contract-welcome-v1";
 import "./caregiver-financial-step-pies-v1.css";
