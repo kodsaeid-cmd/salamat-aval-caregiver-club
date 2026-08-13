0d0eb0af8ed2f52557731de3a2b037d06f282cf4
kodsaeid-cmd <kod.saeid@gmail.com>
2026-08-13 18:58:00 +0330

    feat: mount mobile React caregiver signup

--- mobile-react/caregiver-entry-v5.tsx
+++ mobile-react/caregiver-entry-v5.tsx
@@ -2,6 +2,7 @@ import "../shared/caregiver-mobile-financial-actions-v1";
 import "../shared/caregiver-mobile-scorecard-tabs-v1";
 import "./mobile-ambient-background-v1.css";
 import "./caregiver-scorecard-tabs-v1.css";
+import "./mobile-signup-v1";
 import "./caregiver-v4";
 import "./caregiver-contract-welcome-v1";
 import "./caregiver-contract-progress-rtl-v1.css";
